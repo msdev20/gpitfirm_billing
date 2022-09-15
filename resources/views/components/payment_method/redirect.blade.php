76e17ea0828534c5905951f7375695e2fcde73c6
msdev20 <ms.dev20@gmail.com>
2022-09-15 22:08:07 +0530

    first commit

---
+++ resources/views/components/payment_method/redirect.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="d-none">
+        @if (!empty($setting['name']))
+            <h2>{{ $setting['name'] }}</h2>
+        @endif
+
+        @if (!empty($setting['description']))
+            <div class="well well-sm">{{ $setting['description'] }}</div>
+        @endif
+    </div>
+    <br>
+
+    <div class="buttons">
+        <div class="pull-right">
+            <x-form id="redirect-form" :url="$confirm_url">
+                <button @click="onRedirectConfirm" type="button" id="button-confirm" class="btn disabled:bg-green-100">
+                    {{-- {{ trans('general.confirm') }} --}}
+                    Pay Now
+                </button>
+            </x-form>
+        </div>
+    </div>
+</div>
